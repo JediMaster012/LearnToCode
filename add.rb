778ee490a78fbf9ec8494400fbabf08a08c920d2
Eric Meyer <emeyer@8thlight.com>
2014-08-05 21:09:08 -0500

    Create sample adder

---
+++ add.rb
@@ -0,0 +1,8 @@
+first_number = ARGV[0].to_f
+second_number = ARGV[1].to_f
+
+def add(a, b)
+  return a + b
+end
+
+puts add(first_number, second_number)
--- intro_to_bash.md
+++ intro_to_bash.md
@@ -39,3 +39,8 @@ Bash is a type of shell
 Branching
 Iterating/looping
 Assignment
+
+
+# Assignments
+
+ * 
